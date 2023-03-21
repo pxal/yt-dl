8be9c4513f8b170c814626529a1eb1ed8e8e29d5
Alvian <alvian.1928@yahoo.com>
2023-03-21 18:54:43 +0700

    Update Body.js

---
+++ src/Component/Body.js
@@ -0,0 +1,44 @@
+import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
+import '../App.css';
+
+function Body() {
+    const [link, setLink] = useState("");
+  const [currentLink, setCurrentLink] = useState("");
+
+  function handleChangeText (e) {
+  setLink(e.target.value);
+  }
+
+  function handleSubmit() {
+    setCurrentLink(link);
+  }
+  return (
+    <Card className="text-center">
+      <Card.Header className="Card-Header">Unduh Video</Card.Header>
+      <Card.Body>
+      <div>
+
+
+      <div className="Content">
+      <input type="text" className="Form-yt" placeholder="Tempel Link Youtube Anda Disini"onChange={handleChangeText} />
+     <button className="Yt-submit" onClick={handleSubmit}>Unduh</button>
+      </div>
+
+
+    
+    <iframe className="Widget" 
+    src={`https://convert2mp3s.com/api/widget?url=${currentLink}` }
+    allowtransparency="true" 
+    scrolling="yes" 
+    style={{border: "none"}}
+    ></iframe>
+    </div>
+    
+
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Body;
